53ddf17575173528cf49a5c06e6f5ce7d47ae474
DekiDex <89651615+DekiDex@users.noreply.github.com>
2021-11-16 16:45:18 +0100

    changes two

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,21 +1,22 @@
 const txtName = document.querySelector("#name");
 const btnNotify = document.querySelector("#btn-notify");
-const msgText = document.querySelector("#.text");
-const msgTexttwo = document.querySelector("#.texttwo")
+const msgText = document.querySelector(".text");
+const msgTexttwo = document.querySelector(".texttwo")
 const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 
 btnNotify.addEventListener('click', () => {
-	if(txtName.value.match(mailFormat)  ) {
-		msgText.classList.add('hide');
-		msgTexttwo.classList.add('hide');
-		txtName.removeAttribute('style')
-	
-	}else {
-		msgText.classList.add('hide');
-		msgTexttwo.classList.add('hide');
-		txtName.setAttribute('style', 'border: 2px solid red;');
-	}
+    if(txtName.value.match(mailFormat)  ) {
+        msgText.classList.add('hide');
+        msgTexttwo.classList.add('hide');
+        txtName.removeAttribute('style')
+    
+    }else {
+        msgText.classList.add('hide');
+        msgTexttwo.classList.add('hide');
+        txtName.setAttribute('style', 'border: 2px solid red;');
+    }
 
 
 });
+
