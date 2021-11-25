31b2f84d295264fa90587f3602e8c74df4f4e2bb
DekiDex <89651615+DekiDex@users.noreply.github.com>
2021-11-25 13:24:06 +0100

    changetwo

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,22 +1,41 @@
-const txtName = document.querySelector("#name");
-const btnNotify = document.querySelector('#btn-notify');
-const msgText = document.querySelector('.text-one');
-const msgTexttwo = document.querySelector('.text-two')
-const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const emailInput = document.querySelector('input[name="email"]');
+
+const isValidEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+emailInput.isValid = () => isValidEmail(emailInput.value);
+
+const inputFields = [emailInput];
+
+let shouldValidate = false;
+let isFormValid = false;
+
+const validateInputs = () => {
+  console.log("we are here");
+  if (!shouldValidate) return;
 
 
-btnNotify.addEventListener('click', () => {
-    if(txtName.value.match(mailFormat)  ) {
-        msgText.classList.add('hide');
-        msgTexttwo.classList.add('hide');
-        txtName.removeAttribute('style')
-    
-    }else {
-        msgText.classList.remove('hide');
-        msgTexttwo.classList.remove('hide');
-        txtName.setAttribute('style', 'border: 2px solid red;');
+  isFormValid = true;
+  inputFields.forEach((input) => {
+    input.classList.remove("invalid");
+    emailInput.placeholder = 'example@email/com';
+    input.nextElementSibling.classList.add("hidden");
+
+
+        if (!input.isValid()) {
+      input.classList.add("invalid");
+      isFormValid = false;
+      input.nextElementSibling.classList.remove("hidden");
     }
+  });
+};
 
-
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  shouldValidate = true;
+  validateInputs();
+  if (isFormValid) {
+  }
 });
-
